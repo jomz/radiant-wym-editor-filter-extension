859a3a51a4737995fbf01f58b8cb8524507f71c0
Benny Degezelle <benny@gorilla-webdesign.be>
2008-10-08 22:08:44 +0200

    broke INIT_DIALOG function by changing instance._indexes, using different approach to map instances with parts

--- public/javascripts/radiant.wymeditor.js
+++ public/javascripts/radiant.wymeditor.js
@@ -3,7 +3,7 @@ jQuery.noConflict();
 
 // add load event
 Event.observe(window, 'load', init_load_wym_editor, false);
-
+var editors = new Array();
 // These tokens are for Radiant CMS radius tags  
 //XhtmlLexer.prototype.addTokens = function()
 //{
@@ -71,58 +71,105 @@ function init_load_wym_editor(){
 
 function boot_wym(elem){	
   jQuery(elem).wymeditor({
-				lang: 'nl',
+    lang: 'nl',
+    classesItems: [
+      {'name': 'float_left', 'title': 'PARA: left', 'expr': 'p'},
+      {'name': 'float_right', 'title': 'PARA: right', 'expr': 'p'},
+      {'name': 'maxwidth', 'title': 'PARA: maxwidth', 'expr': 'p'},
+      {'name': 'narrow', 'title': 'PARA: narrow', 'expr': 'p'}
+    ],
+    editorStyles: [
+      {'name': '.float_left',
+       'css': 'color: #999; border: 2px solid #ccc;'},
+      {'name': '.float_right',
+       'css': 'color: #999; border: 2px solid #ccc;'},
+      {'name': '.maxwidth',
+       'css': 'color: #333; border: 2px solid #ccc;'},
+      {'name': '.narrow',
+       'css': 'color: #666; border: 2px solid #CCC;'},
+      {'name': '.radius_tag',
+       'css': 'height:31px; background:url(/images/admin/wef_radiustag_bg.gif) no-repeat 0 0;'},
+      {'name': 'div',
+       'css': 'background:#fafceb url(/images/admin/lbl-div.png) no-repeat 2px 2px; margin:10px; padding:10px;'}
+    ],
+    postInit: function(wym) {
+      // map the index of this instance to it's page_part
+      editors[elem.id] = wym._index;
+      bind_droppability(wym._iframe);
+    },
 
-			 //classes panel
-	      classesItems: [
-	        {'name': 'float_left', 'title': 'PARA: left', 'expr': 'p'},
-	        {'name': 'float_right', 'title': 'PARA: right', 'expr': 'p'},
-	        {'name': 'maxwidth', 'title': 'PARA: maxwidth', 'expr': 'p'},
-	        {'name': 'narrow', 'title': 'PARA: narrow', 'expr': 'p'}
-	      ],
-
-	      //editor css values for visual feedback
-	      editorStyles: [
-	        {'name': '.float_left',
-	         'css': 'color: #999; border: 2px solid #ccc;'},
-	         {'name': '.float_right',
-	          'css': 'color: #999; border: 2px solid #ccc;'},
-	         {'name': '.maxwidth',
-	          'css': 'color: #333; border: 2px solid #ccc;'},
-	         {'name': '.narrow',
-	          'css': 'color: #666; border: 2px solid #CCC;'},
-					{'name': '.radius_tag',
-						'css': 'height:31px; background:url(/images/admin/wef_radiustag_bg.gif) no-repeat 0 0;'},
-						// Make div element stand out visually, add label
- 				{'name': 'div',
- 					'css': 'background:#fafceb url(/images/admin/lbl-div.png) no-repeat 2px 2px; margin:10px; padding:10px;'}
-	      ],
-
-     //function called when WYMeditor instance is ready
-     //wym is the WYMeditor instance
-     postInit: function(wym) {
-       // change the index of this instance into the id string of the parent it has replaced;
-			 wym._index = elem.id;
-			// bind drop function
-			bind_droppability(wym._iframe);
-     },
-
-			preInit: function(wym) {
-				//change_radius_to_imgs
-				var content = (wym._html);
-				var m = content.match(/(<r:([^\/><]*)?\/?>)|(<\/r:([^>]*)?>)/g);
-				if (!(m == null)) {
-					for (var i=0; i < m.length; i++) {
-				  	var title = m[i].replace(/"/g, "'");
-						title = title.substring(1,title.length-1);
-				    var match = escape(m[i].substring(1,m[i].length - 1));
-				    var regex = new RegExp('(' + m[i] + ')', 'i');
-				    var content = content.replace(regex, '<hr class="radius_tag" title="' + title + '" />');
-					}
-				}
-				wym._html = content;
-			}
+		preInit: function(wym) {
+      //change_radius_to_imgs
+      var content = (wym._html);
+      var m = content.match(/(<r:([^\/><]*)?\/?>)|(<\/r:([^>]*)?>)/g);
+      if (!(m == null)) {
+        for (var i=0; i < m.length; i++) {
+          var title = m[i].replace(/"/g, "'");
+          title = title.substring(1,title.length-1);
+          var match = escape(m[i].substring(1,m[i].length - 1));
+          var regex = new RegExp('(' + m[i] + ')', 'i');
+          var content = content.replace(regex, '<hr class="radius_tag" title="' + title + '" />');
+        }
+      }
+      wym._html = content;
+    }
   });
+}
+
+function unboot_wym(elem){
+  // hide wym
+  jQuery(elem).parent().find(".wym_box").remove();
+  // revert images to radius tags
+  var content = elem.value;
+  var regex = new RegExp('<hr class="radius_tag" title="(.*?)" />', 'gi');
+  var m = content.match(regex);
+  if (!(m == null)) {
+    for (var i=0; i<m.length; i++) {
+      var match = unescape(m[i].replace(regex, '<$1>'));
+      var content = content.replace(m[i], match);
+    }
+  }
+  // fix urls to page attachments
+  var regex = new RegExp('src="([\.\/]+)/page_attachments', 'g');
+  var m = content.match(regex);
+  if(!(m == null)) {
+    for(var i=0; i<m.length; i++){
+      var match = unescape(m[i].replace(regex, 'src="/page_attachments'));
+      var content = content.replace(m[i], match)
+    }
+  }
+  // fix urls to assets
+  var regex = new RegExp('src="([\.\/]+)/assets', 'g');
+  var m = content.match(regex);
+  if(!(m == null)) {
+    for(var i=0; i<m.length; i++){
+      var match = unescape(m[i].replace(regex, 'src="/assets'));
+      var content = content.replace(m[i], match)
+    }
+  }
+  elem.value = content;
+  // show textarea again
+  jQuery(elem).show();
+}
+
+function unboot_all_wym() {
+  // save button clicked, update all wym instances
+  if ($('part_0_filter_id')) 								// We're on the page edit screen
+  {
+    for(var i=0;i<$$(".part").length;i++) {
+      // Find all parts that have WYM set as filter
+      filter_select = $("part_" + i + "_filter_id");
+      if(filter_select.value == 'WymEditor'){
+        var index = editors["part_" + i + "_content"];
+        WYMeditor.INSTANCES[index].update();
+      } 
+    }
+  } else if ($('snippet_filter')) { 				// We're on the snippet edit screen
+    if ($F('snippet_filter') == 'WymEditor') {
+      WYMeditor.INSTANCES[0].update();
+    }
+  }
+  return true;
 }
 
 function bind_droppability(box) {
@@ -131,87 +178,20 @@ function bind_droppability(box) {
     onDrop: function(element) {
       var classes = element.className.split(' ');
       var tag_type = classes[0];
-			var link = element.select('a.bucket_link')[0];
-			if(tag_type == 'image'){
-				// copy the original image to WYM
-				var tag = '<img src="'+ link.href +'" alt="'+ link.title +'" />';
-			}
-			else{
-				// copy a link to WYM
-				var asset_id = element.id.split('_').last();
-				var tag = '<a href="'+ link.href +'">'+ link.title +'</a>'
-			}
-      var wym_index = "part_" + (box.ancestors()[2].ancestors()[1].id.split('-')[1] - 1)  + "_content";
-			for(var i=0;i<WYMeditor.INSTANCES.length;i++) {
-				if(WYMeditor.INSTANCES[i]._index != wym_index){ next; }
-				wym = WYMeditor.INSTANCES[i];
-			}
-    	wym.insert(tag);
+      var link = element.select('a.bucket_link')[0];
+      if(tag_type == 'image'){
+        // copy the original image to WYM
+        var tag = '<img src="'+ link.href +'" alt="'+ link.title +'" />';
+      }
+      else{
+        // copy a link to WYM
+        var asset_id = element.id.split('_').last();
+        var tag = '<a href="'+ link.href +'">'+ link.title +'</a>'
+      }
+      var wym_index = editors["part_" + (box.ancestors()[2].ancestors()[1].id.split('-')[1] - 1)  + "_content"];
+      var wymm = WYMeditor.INSTANCES[wym_index];
+      wymm.insert(tag);
     }
   });
   new Draggable('asset-bucket', { starteffect: 'none' });
-}
-
-function unboot_wym(elem){
-	// hide wym
-	jQuery(elem).parent().find(".wym_box").remove();
-	
-	// revert images to radius tags
-	var content = elem.value;
-	var regex = new RegExp('<hr class="radius_tag" title="(.*?)" />', 'gi');
-	var m = content.match(regex);
-	   if (!(m == null)) {
-	       for (var i=0; i<m.length; i++) {
-	           var match = unescape(m[i].replace(regex, '<$1>'));
-	           var content = content.replace(m[i], match);
-	       }
-	   }
-
-	// fix urls to page attachments
-	var regex = new RegExp('src="([\.\/]+)/page_attachments', 'g');
-	var m = content.match(regex);
-	if(!(m == null)) {
-		for(var i=0; i<m.length; i++){
-			var match = unescape(m[i].replace(regex, 'src="/page_attachments'));
-			var content = content.replace(m[i], match)
-		}
-	}
-	// fix urls to assets (paperclipped)
-	var regex = new RegExp('src="([\.\/]+)/assets', 'g');
-	var m = content.match(regex);
-	if(!(m == null)) {
-		for(var i=0; i<m.length; i++){
-			var match = unescape(m[i].replace(regex, 'src="/assets'));
-			var content = content.replace(m[i], match)
-		}
-	}
-	elem.value = content;
-	
-	// show textarea again
-  jQuery(elem).show();
-}
-
-function unboot_all_wym() {
-	// save button clicked, update all wym instances
-	// check to see if we are working with a page or with a snippet
-  if ($('part_0_filter_id'))
-  {
-		// We're on the page edit screen
-    for(var i=0;i<WYMeditor.INSTANCES.length;i++) { 
-			// don't update if the filter for this part is no longer set to wym
-			s = WYMeditor.INSTANCES[i]._index; // i.e. part_0_content
-			filter_select = $("part_" + s.split('_')[1] + "_filter_id");
-			if(filter_select.value == 'WymEditor'){
-				WYMeditor.INSTANCES[i].update();
-				unboot_wym($(s));
-				}
-			}
-  } else if ($('snippet_filter')) {
-		// We're on the snippet edit screen
-    if ($F('snippet_filter') == 'WymEditor') {
-			WYMeditor.INSTANCES[0].update();
-			unboot_wym($$('.textarea')[0]);
-    }
-  }
-	return true;
 }
